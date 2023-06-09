81c2c0491acf88b3c11bac4f409c7e6c2e4a27c7
geethika <geethilka2219@gmail.com>
2023-06-09 06:40:52 -0700

    vpc module

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags                 = merge(var.tags, "${var.env}-vpc")
+  tags                 =var.tags
+#  tags                 = merge(var.tags, "${var.env}-vpc")
 }
 
 #module "subnets" {
