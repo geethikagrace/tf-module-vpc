56abecb645c9ea9b1b80828922fbb6fc79ce724c
geethika <geethilka2219@gmail.com>
2023-06-10 02:20:54 -0700

    vpc module

--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,5 +1,5 @@
-#variable "vpc_id" {}
-#variable "cidr_block" {}
-#variable "tags" {}
-#variable "env" {}
-#variable "name" {}
+variable "vpc_id" {}
+variable "cidr_block" {}
+variable "tags" {}
+variable "env" {}
+variable "name" {}
