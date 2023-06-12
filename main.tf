89d1c1757e4076fe8d4c9c1b6fd8ae86b73cb895
geethika <geethilka2219@gmail.com>
2023-06-12 05:12:06 -0700

    tags

--- main.tf
+++ main.tf
@@ -56,10 +56,10 @@ resource "aws_route" "ngw" {
 
 
 resource "aws_vpc_peering_connection" "peer" {
-  vpc_id        = aws_vpc.main.id
-  peer_vpc_id   = var.default_vpc_id
   peer_owner_id = data.aws_caller_identity.identity.account_id
-  auto_accept   =  true
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept   = true
 
 }
 
