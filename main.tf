843b25938ecd9e1f3f613a9d17f7191f81c34172
geethika <geethilka2219@gmail.com>
2023-06-12 05:46:07 -0700

    rouute table

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_vpc_peering_connection" "peer" {
 
 resource "aws_route" "peering-connection-route" {
   count  = length(local.all_private_subnet_ids)
-  route_table_id = element(element(local.all_private_subnet_ids, count.index ), )
+  route_table_id = element(element(local.all_private_subnet_ids, count.index )
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   destination_cidr_block = var.default_vpc_cidr
 }
